d670b1631702cfb375df16a16a612c947ee7d24e
luisajardim <luisaojardim@gmail.com>
2025-04-08 07:30:38 -0300

    os com arraylist

--- primeira-prova/primeira-prova/primeira-prova/src/main/java/br/lpm/business/OS.java
+++ primeira-prova/primeira-prova/primeira-prova/src/main/java/br/lpm/business/OS.java
@@ -1,55 +1,51 @@
 package br.lpm.business;
 
-public class OS {
-    private static final int MAX_ITENS = 2;
-    private static int contador = 1;
+import java.util.ArrayList;
 
-    private int id;
-    private String nomeCliente;
-    private Item itens[];
-    private int numItens;
-    private float valorTotal;
-    
-public OS(String nomeCliente){
-    this.numItens=0;
-    itens = new Item[MAX_ITENS];
-    this.setNomeCliente(nomeCliente);
-    this.id=contador++;
+public class OS {
+private static int contador = 1;
+
+private int id;
+private String nomeCliente;
+private ArrayList<Item> itens;
+private float valorTotal;
+
+public OS(String nomeCliente) {
+this.itens = new ArrayList<>();
+this.setNomeCliente(nomeCliente);
+this.id = contador++;
 }
 
 public void setNomeCliente(String nomeCliente) {
-    this.nomeCliente=nomeCliente;
+this.nomeCliente = nomeCliente;
 }
 
 public String getNomeCliente() {
-    return nomeCliente;
+return nomeCliente;
 }
 
-public void addItem(Item item){
-    if(numItens<MAX_ITENS){
-        itens[numItens]= item;
-        numItens++;
-    }
+public void addItem(Item item) {
+itens.add(item);
 }
 
-public float calcularTotal(){
-    for(int pos=0; pos<numItens; pos++){
-        valorTotal+=itens[pos].getValor();
-    }
-    return valorTotal;
+public float calcularTotal() {
+valorTotal = 0; // Resetar valorTotal antes de calcular
+for (Item item : itens) {
+valorTotal += item.getValor();
+}
+return valorTotal;
 }
 
 public int getNumItens() {
-    return numItens;
+return itens.size();
 }
 
-public void exibirOS(){
-    System.out.println("ordem: " + id);
-    System.out.println("Nome cliente:" + nomeCliente);
-    for(int pos=0; pos<numItens; pos++){
-        System.out.println("Itens: " + itens[pos].getDescricao());
-    }
-    System.out.println("Valor total: " + calcularTotal());
-
+public void exibirOS() {
+System.out.println("Ordem: " + id);
+System.out.println("Nome cliente: " + nomeCliente);
+for (Item item : itens) {
+System.out.println("Item: " + item.getDescricao());
+}
+System.out.println("Valor total: " + calcularTotal());
 }
 }
